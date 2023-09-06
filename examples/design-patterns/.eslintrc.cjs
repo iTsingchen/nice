c9fa5cb359c9d6b63c9df8d18cf3d0ed4cfccd61
靑琛 <19379305+iTsingchen@users.noreply.github.com>
2023-09-06 09:56:30 -0500

    feat(design-patterns):  新增 design-patterns 项目 (#9)
    close #4
    
    Co-authored-by: anzhuc <anzhuc@keyamedical.com>

---
+++ examples/design-patterns/.eslintrc.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: ['@nice/eslint-config/react'],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  rules: {},
+  overrides: [
+    {
+      files: ['vite.config.ts'],
+      rules: {
+        'import/no-default-export': 'off',
+      },
+    },
+  ],
+};
