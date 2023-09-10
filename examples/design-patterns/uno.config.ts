d8040bcfea7f8ec5b117f8ce6ce5040bbc260002
Qing <626567850@qq.com>
2023-09-10 10:29:29 +0800

    chore(unoCSS-config):  更好的方式配置 unoCSS, 防止幽灵依赖
    （close #17)
    
    fix #17

--- examples/design-patterns/uno.config.ts
+++ examples/design-patterns/uno.config.ts
@@ -1,6 +1,16 @@
 import { defineConfig, presetUno } from 'unocss';
+import type { UserConfig } from 'unocss';
+
+/**
+ * 问: 为什么没有直接使用 export default defineConfig(...) ?
+ * 答:
+ * 1. https://github.com/microsoft/TypeScript/issues/42873
+ * 2. https://github.com/microsoft/TypeScript/issues/47663#issuecomment-1519138189
+ */
 
 // https://unocss.dev/guide/config-file
-export default defineConfig({
+const config: UserConfig = defineConfig({
   presets: [presetUno()],
 });
+
+export default config;
